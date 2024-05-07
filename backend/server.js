0b34d9d18a09593a32100edc561234d8d10ba145
Robin Malik <robin.malik@alcortech.com>
2024-05-07 09:42:36 +0530

    Added Multer and its Configuration

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const multer = require("multer");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
 const app = express();
 
-//cors middleware
+// Multer configuration for file upload
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, filesDir);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+});
+
+const upload = multer({ storage: storage });
+
+//Cors middleware
 app.use(cors());
 
-//config
 //Config
 dotenv.config({ path: "backend/config/config.env" });
 
