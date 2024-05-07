3d78eb1458c8516d39ed26d8c23d17369912d9fa
Robin Malik <robin.malik@alcortech.com>
2024-05-07 09:50:14 +0530

    Added POST method for uploading a file in chunks

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const multer = require("multer");
+const fs = require("fs"); 
 const cors = require("cors");
 const dotenv = require("dotenv");
 
 const app = express();
+
+//Config
+dotenv.config({ path: "backend/config/config.env" });
+
+const filesDir = './files';
+if (!fs.existsSync(filesDir)){
+  fs.mkdirSync(filesDir);
+}
 
 // Multer configuration for file upload
 const storage = multer.diskStorage({
@@ -21,8 +30,15 @@ const upload = multer({ storage: storage });
 //Cors middleware
 app.use(cors());
 
-//Config
-dotenv.config({ path: "backend/config/config.env" });
+app.post("/uploadChunk", upload.single("chunk"), (req, res, next) => {
+  const { path, originalname: filename } = req.file;
+  res.send({
+    success: true,
+    message: "Chunk has been uploaded successfully",
+    data: { path, filename },
+  });
+});
+
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
